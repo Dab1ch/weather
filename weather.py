89a0248f8719e302c76dfa406b0f406eb8d4f174
Dab1ch <103375317+Dab1ch@users.noreply.github.com>
2023-03-13 23:05:46 +0300

    Add files via upload

--- weather.py
+++ weather.py
@@ -1,20 +1,27 @@
 from flask import Flask, session, request, redirect, url_for, render_template
 import os
-
+from bs4 import BeautifulSoup as bs
+import requests
 
 def choose_city():
 	
 	if request.method == 'POST':
 		session['city'] = request.form.get('city')
 		
-		if session['city'] == 0:
-			return redirect(url_for('choose_city'))
 		return redirect(url_for('view'))
 	
 	return render_template('start.html')
 
 def view():
-	pass
+	
+	URL_TEMPLATE = "https://meteoinfo.ru/forecasts/russia/moscow-area/moscow"
+	r = requests.get(URL_TEMPLATE)
+	
+	index = r.text.find('<table')
+	
+	print(r.text[index + 20:index + 40])
+	
+	return render_template('finish.html', index = index)
 
 folder = os.getcwd()
 app = Flask(__name__, template_folder = folder, static_folder = folder)
