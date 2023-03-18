a9180a2e72d4bd43c397e753a8e885e3b889c081
Dab1ch <103375317+Dab1ch@users.noreply.github.com>
2023-03-18 13:13:55 +0300

    Add files via upload

--- weather.py
+++ weather.py
@@ -2,6 +2,8 @@ from flask import Flask, session, request, redirect, url_for, render_template
 import os
 from bs4 import BeautifulSoup as bs
 import requests
+from datetime import date
+
 
 def choose_city():
 	
@@ -13,15 +15,23 @@ def choose_city():
 	return render_template('start.html')
 
 def view():
-	
+
+	current_date = date.today()
+	print(current_date.day, current_date.month)
+
 	URL_TEMPLATE = "https://meteoinfo.ru/forecasts/russia/moscow-area/moscow"
 	r = requests.get(URL_TEMPLATE)
 	
-	index = r.text.find('<table')
-	
-	print(r.text[index + 20:index + 40])
-	
-	return render_template('finish.html', index = index)
+	r = bs(r.text, "html.parser")
+
+	temps = r.findAll('span', class_ = 'fc_temp_short')
+	temps = ''.join(map(str, temps))
+	temps = temps.split('<i>')
+	temps.pop(0)
+
+
+	#print(r.text[index + 20:index + 40])
+	return render_template('finish.html', temps = temps)
 
 folder = os.getcwd()
 app = Flask(__name__, template_folder = folder, static_folder = folder)
