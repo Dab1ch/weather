a36350fbc111a60739ef00159f1a6110e5deeaf2
Dab1ch <103375317+Dab1ch@users.noreply.github.com>
2023-03-21 16:28:25 +0300

    Add files via upload

--- weather.py
+++ weather.py
@@ -5,6 +5,12 @@ import requests
 from datetime import date
 
 
+months = ['январь', 'февраль', "март", "апрель", "май", "июнь", "июль",
+"август", "сентябрь", "октябрь", "ноябрь", "декабрь"]
+
+monthdays = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
+
+
 def choose_city():
 	
 	if request.method == 'POST':
@@ -19,6 +25,10 @@ def view():
 	current_date = date.today()
 	print(current_date.day, current_date.month)
 
+	day = current_date.day
+	for i in range(current_date.month - 1):
+		day += monthdays[i]
+
 	URL_TEMPLATE = "https://meteoinfo.ru/forecasts/russia/moscow-area/moscow"
 	r = requests.get(URL_TEMPLATE)
 	
@@ -29,9 +39,23 @@ def view():
 	temps = temps.split('<i>')
 	temps.pop(0)
 
+	for i in range(len(temps)):
+		temps[i] = temps[i][:temps[i].find('</i>')]
+	print(temps)
 
-	#print(r.text[index + 20:index + 40])
-	return render_template('finish.html', temps = temps)
+	days = []
+
+	for i in range(6):
+		for j in range(len(monthdays)):
+			day = day % 365
+			if monthdays[j] > day:
+				days.append(str(months[j]) + ' ' + str(day - monthdays[j - 1]))
+				break 
+		day+=1
+
+
+	#print(r.text[index + 20:index + 40])	
+	return render_template('finish.html', days = days, temps = temps)
 
 folder = os.getcwd()
 app = Flask(__name__, template_folder = folder, static_folder = folder)
